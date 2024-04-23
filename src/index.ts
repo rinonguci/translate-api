a73a813df0ed0be9baebd65f3f2f1598ded130f4
rinonguci <phatphamm01@gmail.com>
2024-04-23 16:34:50 +0700

    hihi

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,26 @@
-const translate = require('@vitalets/google-translate-api');
-const express = require('express');
-const cors = require('cors');
+const translate = require("free-google-translator-api");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 1234;
 
 app.use(cors());
 
-app.options('*', cors());
+app.options("*", cors());
 
-app.get('/', async (req, res) => {
-  const params = ['from', 'to', 'text'];
+app.get("/", async (req, res) => {
+  try {
+    const { from = "auto", to = "vi", text = "Hello world" } = req.query;
 
-  if (!req.query || (req.query && Object.keys(req.query).length === 0)) {
-    res.send('Null');
+    const data = await translate(text, from, to);
+
+    console.log("🚀 ~ app.get ~ data:", data);
+    res.status(200).json({
+      text: data,
+    });
+  } catch (error) {
+    console.log("🚀 ~ app.get ~ error:", error);
   }
-
-  const { from = 'auto', to = 'vi', text = 'Hello world' } = req.query;
-
-  const data = await translate(text, {
-    from,
-    to,
-  });
-
-  res.send({ text: data.text });
 });
 
 app.listen(port, () => {
